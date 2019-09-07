231d525c7426a9aac5a372f9f1eff7c7f51a7b5a
isisisisisitch <dallucus@gmail.com>
2019-09-07 12:25:36 -0400

    added my first project

---
+++ src/com/luoExpress/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.luoExpress.demo;
+
+public class HelloWorld {
+
+        //entry
+        //String[] args  system args
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
