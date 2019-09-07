744b59c7a0433e709275d26e9cbc38071117a2ed
isisisisisitch <dallucus@gmail.com>
2019-09-07 12:52:13 -0400

    add new code

--- src/com/luoExpress/demo/HelloWorld.java
+++ src/com/luoExpress/demo/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         //String[] args  system args
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("add new code");
     }
 }
